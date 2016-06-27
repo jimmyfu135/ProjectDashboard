af224ec1b8a142110f6792b06485a5ca8e55a313
jimmyfu <jimmyfu135@hotmail.com>
2016-06-27 18:34:28 +0800

    提交首页功能

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -10,37 +10,33 @@ AppAsset::addMultipleSelect($this,'@web/js/multiplescript.js','@web/css/multiple
 $this->registerJsFile('@web/js/projectplan.js',['depends' => ['backend\assets\AppAsset']]);
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-    <div class="body-content">
-        <div class="row">
-            <div class="demo">
-                <ul class="select">
-                    <li class="select-list">
-                        <dl id="select1">
-                            <dt>视图：</dt>
-                            <dd class="select-all selected"><a href="#">需求视图</a></dd>
-                            <dd><a href="#">资源视图</a></dd>
-                        </dl>
-                    </li>
-                    <li class="select-list">
-                        <dl id="select2">
-                            <dt>ABU：</dt>
-                            <dd class="select-all selected"><a href="#">上海一组</a></dd>
-                            <dd><a href="#">上海二组</a></dd>
-                            <dd><a href="#">上海三组</a></dd>
-                        </dl>
-                    </li>
-                    <li class="select-result">
-                        <dl>
-                            <dt>已选条件：</dt>
-                            <dd class="select-no">暂时没有选择过滤条件</dd>
-                        </dl>
-                    </li>
-                </ul>
-            </div>
 
-            <div id='calendar'></div>
-        </div>
+<ul class="select">
+    <li class="select-list">
+        <dl id="select2">
+            <dt>ABU：</dt>
+            <dd class="select-all selected"><a href="#">上海一组</a></dd>
+            <dd><a href="#">上海二组</a></dd>
+            <dd><a href="#">上海三组</a></dd>
+        </dl>
+    </li>
+</ul>
+
+<ul class="nav nav-tabs">
+    <li class="active">
+        <a href="#tab_requirementCalendar" data-toggle="tab">需求视图</a>
+    </li>
+    <li>
+        <a id="navResource" href="#tab_resourceCalendar" data-toggle="tab">资源视图</a>
+    </li>
+</ul>
+
+<div id="myTabContent" class="tab-content">
+    <div style="margin-top: 5px;" class="tab-pane fade in active" id='tab_requirementCalendar'>
+        <div id="requirementCalendar"></div>
+    </div>
+    <div style="margin-top: 5px;" class="tab-pane fade" id='tab_resourceCalendar'>
+        <div id="resourceCalendar"></div>
     </div>
 </div>
 
