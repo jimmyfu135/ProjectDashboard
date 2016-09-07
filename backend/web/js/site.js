0d5ba030806e75be43922addc704c88c19f41cd8
付进 <fuj01@mysoft.com.cn>
2016-09-07 21:40:27 +0800

    修复BUG

--- backend/web/js/site.js
+++ backend/web/js/site.js
@@ -169,22 +169,6 @@ $(document).ready(function() {
     $("#navRequirement").on('shown.bs.tab',function(e) {
         $('#requirementCalendar').fullCalendar('refetchEvents');
     });
-    //默认选中
-    var sCurrentDepartment="0";
-    $.ajax({
-        url: 'index.php?r=site/get-current-department',
-        type: 'get',
-        data: "",
-        async:false,
-        success: function (data) {
-            sCurrentDepartment=data;
-        }
-    });
-    if(sCurrentDepartment=="1" || sCurrentDepartment=="2" || sCurrentDepartment=="3"){
-        //$("#depart"+sCurrentDepartment).addClass("active");
-        $("#depart"+sCurrentDepartment).trigger("click");
-    }else {
-        $("#depart0").trigger("click");
-    }
+    
 
 });
